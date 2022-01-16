7f7ce5de6033a7e0b87daf382a4f8274fb0d29e2
Kevin Cheng <qkevcheng@fb.com>
2022-01-15 22:04:25 -0800

    test

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ app.get('/webhooks', function(req, res) {
 
 app.post('/webhooks', function(req, res) {
   var isValid = req.isXHubValid();
-  console.log('isValid: '.isValid);
+  console.log('isValid: ' + isValid);
   // if (!req.isXHubValid()) {
   //   console.log('Received webhooks update with invalid X-Hub-Signature');
   //   res.sendStatus(401);
